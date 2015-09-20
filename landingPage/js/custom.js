a261b4ac2044e351481037a7197b1f495dc7f37b
rubinz <zevirc@gmail.com>
2015-09-20 13:29:35 -0400

    chaing landing page test

--- landingPage/js/custom.js
+++ landingPage/js/custom.js
@@ -8,7 +8,7 @@
  Sticky Navbar
  =============================================== */
 
-$(document).ready(function() {
+$(document).ready(function () {
     $(".navbar").sticky({topSpacing: 0});
 });
 
@@ -17,7 +17,7 @@ $(document).ready(function() {
 /* ==============================================
  main flex slider
  =============================================== */
-$(window).load(function() {
+$(window).load(function () {
     $('.main-flex-slider').flexslider({
         slideshowSpeed: 5000,
         directionNav: false,
@@ -33,7 +33,7 @@ $(window).load(function() {
 
 function close_toggle() {
     if ($(window).width() <= 768) {
-        $('.navbar-collapse a').on('click', function() {
+        $('.navbar-collapse a').on('click', function () {
             $('.navbar-collapse').collapse('hide');
         });
     }
@@ -76,7 +76,7 @@ $('.chart').each(function () {
 });
 
 //owl carousel for testimonials
-$(document).ready(function() {
+$(document).ready(function () {
 
     $("#testi-carousel,#work-slide").owlCarousel({
         // Most important owl features
@@ -106,7 +106,7 @@ $('#grid').mixitup();
 
 
 /************parallax*********************/
-$(function() {
+$(function () {
     $.stellar({
         horizontalScrolling: false
     });
@@ -116,7 +116,7 @@ $(function() {
 /* ==============================================
  Counter Up
  =============================================== */
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     $('.counter').counterUp({
         delay: 100,
         time: 800
@@ -144,53 +144,61 @@ $('.show-image').magnificPopup({type: 'image'});
 
 
 //smooth scroll
-$(function() {
-	  $('.scrollto a[href*=#]:not([href=#])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$(function () {
+    $('.scrollto a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html,body').animate({
-	          scrollTop: target.offset().top - 50
-	        }, 1000);
-	        return false;
-	      }
-	    }
-	  });
-	});	
-        
-        
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 50
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
 
 // Backstretch - Slider on Background
-								  
-			 
+
+
 $(".fullscreen").backstretch([
-   "img/showcase-5.jpg",
-   "img/showcase-2.jpg",
-   "img/showcase-3.jpg"
+    "img/showcase-5.jpg",
+    "img/showcase-2.jpg",
+    "img/showcase-3.jpg"
 ], {duration: 5000, fade: 1000});
 
 
 //back to top
 
-$(document).ready(function(){
-	
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 800) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-	
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-	
+$(document).ready(function () {
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 800) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function () {
+        $('html, body').animate({scrollTop: 0}, 800);
+        return false;
+    });
+
+});
+
+/* ==============================================
+ mb.YTPlayer
+ =============================================== */
+
+jQuery(function () {
+    jQuery(".player").mb_YTPlayer();
 });
 
 
